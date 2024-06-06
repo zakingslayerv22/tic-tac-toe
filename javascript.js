3694081ad27468f891dffed7743a425528b6d4a8
2 Sam 22:38-43 <11926638+zakingslayerv22@users.noreply.github.com>
2024-06-06 14:12:02 +0200

    Create createCell() and printBoard() method
    First change the createGameBoard() from an IIFE
    of module pattern to a normal factory function
    for uniformity with the createCell() function.
    
    With an IIFE, a const variable is not initialized
    until its declaration at runtime so createCell()
    will not be able be called before its declaration.
    
    The createCell() defines a default cell value of 0.
    This will make the default cell value anywhere
    the function is called to be 0. It also
    returns the value with the getCellValue() method.
    
    In the createGameBoard(), the for loop pushes the
    createCell() into the board array instead of 0.
    This is because the createCell() will contain
    useful methods for handling cell content.
    
    Edit the addMarker() method such that the if
    statement uses the getCellValue() method to check
    if the value of the cell is 0 and if so, logs
    something to the console (for now). Able to call
    getCellValue() here because createCell() was
    called in the for loop above and thus getCellValue()
    has closure. Each createCell() retains access
    to its properties and methods due to closure.
    
    Define a printBoard() method that will go through
    all the elements of board (rows), go through all
    the elements of rows and use the getCellValue()
    to get the value of all elements in each row.
    This was done because getBoard() now console
    log createCell() as it is createCell() that
    is now being pushed into each cell.
    
    Set the createGameBoard() to the gameBoard constant
    and call the printBoard() method. Also call the
    addMarker() method to verify if it logs the string
    that was set.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 //this function represents the state of the board
 //each square would hold a cell() (defined later)
 
-const createGameBoard = (function() {
+function createGameBoard() {
     const rows = 3;
     const columns = 3;
     const board = [];
@@ -11,7 +11,7 @@ const createGameBoard = (function() {
     for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
-            board[i].push(0);
+            board[i].push(createCell());
         }
     }
 
@@ -19,22 +19,37 @@ const createGameBoard = (function() {
     const getBoard = () => board;
 
     const addMarker = (row, column) => {
-        if (board[row][column] === 0) {
-            board[row][column] = "x";
+        if (board[row][column].getCellValue() === 0) {
+            //do something here
+            console.log("Sub Zero!")
+            //board[row][column] = "x";
         } else {
             return;
         }
     }
 
-    return { getBoard, addMarker }
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getCellValue()));
+        console.log(boardWithCellValues)
+    }
 
-})();
+    return { getBoard, addMarker, printBoard }
 
+}
 
+function createCell() {
+    //default cell value
+    let cellValue = 0;
 
+    //get the value of the cell
+    const getCellValue = () => cellValue;
 
-console.log(createGameBoard.getBoard());
+    return { getCellValue }
+}
 
-createGameBoard.addMarker(0, 1);
-createGameBoard.addMarker(2, 1);
-createGameBoard.addMarker(2, 1);
+const gameBoard = createGameBoard()
+gameBoard.printBoard()
+
+gameBoard.addMarker(0, 1);
+// gameBoard.addMarker(2, 1);
+// gameBoard.addMarker(2, 1);
